f12f216049eae5570b625ded99c9e60cc0b8c01f
Isaac Braun <ibraun@esri.com>
2025-07-09 16:57:30 -0700

    add function to get Monday ID

--- .github/scripts/updateMondayDate.js
+++ .github/scripts/updateMondayDate.js
@@ -5,19 +5,53 @@
 /** @param {import('github-script').AsyncFunctionArguments} AsyncFunctionArguments */
 module.exports = async ({ github, context }) => {
   const { repo, owner } = context.repo;
+  const { MONDAY_KEY } = process.env;
   const issue = /** @type {import('@octokit/webhooks-types').IssuesMilestonedEvent} */ (context.payload?.issue);
+
+  function findMondayID(githubID) {
+    const query = `query ($boardID: Number!, $columnID: String!, $githubID: String!) {
+      items_page_by_column_values(
+        board_id: $boardID,
+        columns: {
+          column_id: $columnID,
+          column_values: [$githubID]
+        },
+      ) {
+        items {
+          id
+        }
+      }
+    }`;
+    const vars = {
+      "boardID": 8780429793,
+      "columnID": "numeric_mknk2xhh",
+      "githubID": githubID,
+    };
+
+    fetch ("https://api.monday.com/v2", {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization' : MONDAY_KEY,
+      },
+      body: JSON.stringify({
+        'query' : query,
+        'variables' : JSON.stringify(vars)
+      })
+    })
+      .then(res => res.json())
+      .then(res => console.log(JSON.stringify(res, null, 2)));
+      }
+  }
 
   if (issue && issue.milestone) {
     const milestone = issue.milestone;
     
-    console.log("A milestone was created/updated");
-    console.log("Milestone Title:", milestone.title);
-    console.log("Milestone Description:", milestone.description);
-    console.log("Milestone Due Date:", milestone.due_on); // If applicable
+    console.log(`A milestone was created/updated on issue #${issue.number}`);
+    console.log(`Milestone Due Date: ${milestone.due_on}`);
 
-    // Add your logic here to update the due date in Monday.com
-    
+    console.log(`Monday ID: ${findMondayID(issue.number)});
   } else {
-    console.log(`No milestone found for issue #${payload.issue.number}.`);
+    console.log(`No milestone found for issue #${issue.number}.`);
   }
 };
