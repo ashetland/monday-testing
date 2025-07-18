f6d86d8e9bd81972f88fdf857136cb648dc53085
Isaac Braun <isaacbraun2000@gmail.com>
2025-07-18 10:58:33 -0700

    testing monday task creation through API

---
+++ .github/scripts/createMondayTask.js
@@ -0,0 +1,143 @@
+// @ts-check
+/** @param {import('github-script').AsyncFunctionArguments} AsyncFunctionArguments */
+module.exports = async ({ context }) => {
+  const BOARD = "8780429793";
+  const columns = {
+    issue_id: "numeric_mknk2xhh",
+    date: "date6",
+    link: "link",
+    people: "people",
+    status: "dup__of_overall_status__1",
+    issue_type: "color_mksw3bdr",
+    priority: "priority",
+  };
+  const { MONDAY_KEY } = process.env;
+
+  const payload = /** @type {import('@octokit/webhooks-types').IssuesCreatedEvent} */ (context.payload);
+
+  /**
+   * @typedef {object} Issue
+   * @property {string} title
+   * @property {string} url
+   * @property {string} body
+   * @property {string} number
+   * @property {object} milestone
+   * @property {string} milestone.title
+   * @property {People[]} assignees
+   * @property {Label[]} labels
+   */
+
+  /**
+   * @typedef {object} People
+   * @property {string} login
+   * @property {string} id
+   * @property {string} node_id
+   * @property {string} avatar_url
+   * @property {string} url
+   * @property {string} html_url
+   * @property {string} type
+   * @property {boolean} site_admin
+   */
+
+  /**
+   * @typedef {object} Label
+   * @property {string} name
+   * @property {string} color
+   * @property {string} description
+   * @property {string} url
+   * @property {string} id
+   * @property {string} node_id
+   * @property {boolean} default
+   */
+
+  /** @type {Issue} */
+  const {
+    title,
+    url,
+    body,
+    number,
+    labels,
+    assignees,
+    milestone: {
+      title: milestone
+    }
+  } = payload.issue;
+
+  /**
+   * Calls the Monday.com API with a provided query
+   * @param {string} query
+   * @returns {Promise<string | undefined>}
+   */
+  async function callMonday(query) {
+    try {
+      const response = await fetch("https://api.monday.com/v2", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: MONDAY_KEY,
+        },
+        body: JSON.stringify({
+          query: query,
+        }),
+      });
+
+      const body = await response.json();
+
+      if (!response.ok) {
+        console.log(body);
+        throw new Error(`HTTP error when callid the Monday API: ${body}`);
+      }
+
+      return body;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const issueTypeLabels = ["bug", "enhancement", "a11y", "docs", "refactor", "spike", "testing", "tooling"];
+  const statusLabels = ["needs triage", "needs milestone", "ready for dev", "1 - assigned", "2 - in development", "3 - installed", "4 - verified"];
+  const priorityLabels = ["p - high", "p - medium", "p - low"];
+
+  let issueType, status, priority = "";
+
+  const people = assignees.map((assignee) => {
+    return `{"id": "${assignee.id}"}`;
+  }).join(", ");
+
+  for (const label of labels) {
+    if (issueTypeLabels.includes(label.name)) {
+      issueType = label.name;
+      break;
+    }
+    if (statusLabels.includes(label.name)) {
+      status = label.name;
+      break;
+    }
+    if (priorityLabels.includes(label.name)) {
+      priority = label.name;
+      break;
+    }
+  }
+
+  const columnValues = JSON.stringify(`{
+    "${columns.id}": ${number},
+    "${columns.link}": "${url}",
+    "${columns.people}": [${people}],
+    "${columns.status}": "${status || "needs triage"}",
+    "${columns.issue_type}": "${issueType}",
+    "${columns.priority}": "${priority}",
+  }`);
+
+  const query = `mutation { 
+    create_item (
+      board_id: ${BOARD},
+      item_name: \"${title}\",
+      column_values: ${columnValues}
+    ) {
+      id
+    }
+  }`;
+
+  const id = callMonday(query);
+  console.log(`Created Monday.com task with ID: ${id}`);
+};
