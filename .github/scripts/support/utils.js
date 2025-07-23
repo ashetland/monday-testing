8feda47535182544055e5b00f231fa424a9a80c0
Isaac Braun <isaacbraun2000@gmail.com>
2025-07-23 15:00:24 -0700

    feat: refactor utils into functions and add handleMilestone

--- .github/scripts/support/utils.js
+++ .github/scripts/support/utils.js
@@ -1,173 +1,278 @@
 // @ts-check
-const { mondayLabels, mondayPeople } = require("./resources");
+const {
+  mondayBoard,
+  mondayColumns,
+  mondayLabels,
+  mondayPeople,
+  resources,
+} = require("./resources");
+
+/**
+ * @typedef {object} removeLabelParam
+ * @property {InstanceType<typeof import('@actions/github/lib/utils').GitHub>} github
+ * @property {import('@actions/github/lib/context').Context} context
+ * @property {string} label
+ *
+ * @param {removeLabelParam} obj
+ **/
+async function removeLabel({ github, context, label }) {
+  const { owner, repo } = context.repo;
+  const issue_number = context.issue.number;
+
+  try {
+    await github.rest.issues.removeLabel({
+      issue_number,
+      owner,
+      repo,
+      name: label,
+    });
+  } catch (err) {
+    if (err.status === 404) {
+      console.log(
+        `The label '${label}' is not associated with issue #${issue_number}.`,
+        err,
+      );
+    } else {
+      console.log("Error while attempting to remove issue label.", err);
+    }
+  }
+}
+
+/**
+ * @typedef {object} createLabelIfMissingParam
+ * @property {InstanceType<typeof import('@actions/github/lib/utils').GitHub>} github
+ * @property {import('@actions/github/lib/context').Context} context
+ * @property {string} label
+ * @property {string} color
+ * @property {string} description
+ *
+ * @param {createLabelIfMissingParam} obj
+ **/
+async function createLabelIfMissing({
+  github,
+  context,
+  label,
+  color,
+  description,
+}) {
+  const { owner, repo } = context.repo;
+  try {
+    await github.rest.issues.getLabel({
+      owner,
+      repo,
+      name: label,
+    });
+  } catch {
+    await github.rest.issues.createLabel({
+      owner,
+      repo,
+      name: label,
+      color,
+      description,
+    });
+  }
+}
+
+/**
+ * Calls the Monday.com API with a provided query
+ * @param {string | undefined} key - The Monday.com API key
+ * @param {string} query - The GraphQL query to execute
+ * @returns {Promise<string | undefined>}
+ */
+async function callMonday(key, query) {
+  try {
+    if (!key) {
+      throw new Error("Monday.com API key is not set.");
+    }
+
+    const response = await fetch("https://api.monday.com/v2", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: key,
+      },
+      body: JSON.stringify({
+        query: query,
+      }),
+    });
+
+    const body = await response.json();
+
+    if (!response.ok) {
+      console.log(body);
+      throw new Error(`HTTP error when callid the Monday API: ${body}`);
+    }
+
+    return body;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+/**
+ * Returns the Monday.com task ID for the passed GitHub Issue ID.
+ * Matches based on the GitHub Issue ID column.
+ * @param {number} githubID
+ * @returns {Promise<string>}
+ */
+async function getMondayID(key, githubID) {
+  const query = `query {
+      items_page_by_column_values(
+        board_id: "${mondayBoard}",
+        columns: {
+          column_id: "${mondayColumns.issueNumber}",
+          column_values: ["${githubID}"]
+        },
+      ) {
+        items {
+          id
+        }
+      }
+    }`;
+
+  const response = await callMonday(key, query);
+
+  if (!response) {
+    throw new Error(`No response for Github Issue #${githubID}`);
+  }
+
+  const items = response["data"]["items_page_by_column_values"]["items"];
+
+  if (!items?.length) {
+    throw new Error(`No items found for Github Issue #${githubID}`);
+  }
+
+  return items[0]["id"];
+}
+
+/**
+ * Inserts or replaces the Monday sync line in the issue body string
+ * @param {string | null} body - The current issue body
+ * @param {string} mondayID - The Monday.com item ID
+ * @returns {string} - The updated issue body
+ */
+function addSyncLine(body, mondayID) {
+  const syncMarkdown = `**monday.com sync:** #${mondayID}\n\n`;
+  const syncLineRegex = /^\*\*monday\.com sync:\*\* #\d+\n\n?/m;
+  if (body && syncLineRegex.test(body)) {
+    return body.replace(syncLineRegex, syncMarkdown);
+  } else {
+    return syncMarkdown + (body || "");
+  }
+}
+
+/**
+ * Assigns labels to the Monday.com task object based on the issue labels
+ * @param {import('@octokit/webhooks-types').Label[]} labels - The labels from the issue
+ * @param {object} values - The current column values object to update
+ * @returns {object} - The updated column values object
+ */
+function assignLabels(labels, values) {
+  for (const label of labels) {
+    // TEMP
+    if (label.name === "monday.com sync") {
+      // Skip the sync label, as it is not needed in Monday.com
+      continue;
+    }
+
+    if (!mondayLabels.has(label.name)) {
+      console.warn(`Label ${label.name} not found in Monday Labels map`);
+      continue;
+    }
+
+    const info = mondayLabels.get(label.name);
+    if (!info?.column || !info?.value) {
+      console.warn(
+        `Label ${label.name} is missing column or title information`,
+      );
+      continue;
+    }
+
+    if (!values[info.column]) {
+      values[info.column] = info.value;
+    } else {
+      values[info.column] += `, ${info.value}`;
+    }
+  }
+
+  return values;
+}
+
+/**
+ * Assigns a person to the Monday.com task object based on their GitHub username/role
+ * @param {import('@octokit/webhooks-types').User} person
+ * @param {object} values - The current column values object to update
+ * @returns {object} - The updated column values object
+ */
+function assignPerson(person, values) {
+  if (!person?.login) {
+    console.warn("No person or login provided for assignment");
+    return;
+  }
+
+  const info = mondayPeople.get(person.login);
+
+  if (!info) {
+    console.warn(`Assignee ${person.login} not found in peopleMap`);
+    return;
+  }
+
+  if (!values[info.role]) {
+    values[info.role] = `${info.id}`;
+  } else {
+    values[info.role] += `, ${info.id}`;
+  }
+
+  return values;
+}
+
+/**
+ * Returns column and value to update from a milestone title
+ * @param {string} milestone - The title of the milestone
+ * @returns {{ column: string, value: string }} - The column ID and value to update in Monday.com
+ */
+function handleMilestone(milestone) {
+  //
+  /**
+   * Formats a date string for Monday.com
+   * @param {string} date - The date string to format
+   * @returns { string } - The formatted date string
+   */
+  const formatDate = (date) => JSON.stringify(JSON.stringify({ date: date }));
+
+  // Attempt to extract the date from the milestone title
+  const dateRegex = /\d{4}-\d{2}-\d{2}/;
+  const dueDate = milestone.match(dateRegex);
+
+  if (dueDate) {
+    return {
+      column: mondayColumns.date,
+      value: formatDate(dueDate[0]),
+    };
+  }
+
+  // resources.milestone.backlog, resources.milestone.freezer
+  const statusMilestones = [resources.milestone.stalled];
+  if (statusMilestones.includes(milestone)) {
+    return {
+      column: mondayColumns.status,
+      value: milestone,
+    };
+  }
+
+  return {
+    column: mondayColumns.date,
+    value: formatDate(""),
+  };
+}
 
 module.exports = {
-  /**
-   * @typedef {object} removeLabelParam
-   * @property {InstanceType<typeof import('@actions/github/lib/utils').GitHub>} github
-   * @property {import('@actions/github/lib/context').Context} context
-   * @property {string} label
-   *
-   * @param {removeLabelParam} obj
-   **/
-  removeLabel: async ({ github, context, label }) => {
-    const { owner, repo } = context.repo;
-    const issue_number = context.issue.number;
-
-    try {
-      await github.rest.issues.removeLabel({
-        issue_number,
-        owner,
-        repo,
-        name: label,
-      });
-    } catch (err) {
-      if (err.status === 404) {
-        console.log(`The label '${label}' is not associated with issue #${issue_number}.`, err);
-      } else {
-        console.log("Error while attempting to remove issue label.", err);
-      }
-    }
-  },
-
-  /**
-   * @typedef {object} createLabelIfMissingParam
-   * @property {InstanceType<typeof import('@actions/github/lib/utils').GitHub>} github
-   * @property {import('@actions/github/lib/context').Context} context
-   * @property {string} label
-   * @property {string} color
-   * @property {string} description
-   *
-   * @param {createLabelIfMissingParam} obj
-   **/
-  createLabelIfMissing: async ({ github, context, label, color, description }) => {
-    const { owner, repo } = context.repo;
-    try {
-      await github.rest.issues.getLabel({
-        owner,
-        repo,
-        name: label,
-      });
-    } catch {
-      await github.rest.issues.createLabel({
-        owner,
-        repo,
-        name: label,
-        color,
-        description,
-      });
-    }
-  },
-
-  /**
-   * Calls the Monday.com API with a provided query
-   * @param {string | undefined} key - The Monday.com API key
-   * @param {string} query - The GraphQL query to execute
-   * @returns {Promise<string | undefined>}
-   */
-  callMonday: async (key, query) => {
-    try {
-      if (!key) {
-        throw new Error("Monday.com API key is not set.");
-      }
-
-      const response = await fetch("https://api.monday.com/v2", {
-        method: "post",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: key,
-        },
-        body: JSON.stringify({
-          query: query,
-        }),
-      });
-
-      const body = await response.json();
-
-      if (!response.ok) {
-        console.log(body);
-        throw new Error(`HTTP error when callid the Monday API: ${body}`);
-      }
-
-      return body;
-    } catch (error) {
-      console.log(error);
-    }
-  },
-  /**
-   * Inserts or replaces the Monday sync line in the issue body string
-   * @param {string | null} body - The current issue body
-   * @param {string} mondayID - The Monday.com item ID
-   * @returns {string} - The updated issue body
-   */
-  addSyncLine: (body, mondayID) => {
-    const syncMarkdown = `**monday.com sync:** #${mondayID}\n\n`;
-    const syncLineRegex = /^\*\*monday\.com sync:\*\* #\d+\n\n?/m;
-    if (body && syncLineRegex.test(body)) {
-      return body.replace(syncLineRegex, syncMarkdown);
-    } else {
-      return syncMarkdown + (body || '');
-    }
-  },
-  /**
-   * Assigns labels to the Monday.com task object based on the issue labels
-   * @param {import('@octokit/webhooks-types').Label[]} labels - The labels from the issue
-   * @param {object} values - The current column values object to update
-   * @returns {object} - The updated column values object
-   */
-  assignLabels: (labels, values) => {
-    for (const label of labels) {
-      // TEMP
-      if (label.name === "monday.com sync") {
-        // Skip the sync label, as it is not needed in Monday.com
-        continue;
-      }
-
-      if (!mondayLabels.has(label.name)) {
-        console.warn(`Label ${label.name} not found in Monday Labels map`);
-        continue;
-      }
-
-      const info = mondayLabels.get(label.name);
-      if (!info?.column || !info?.value) {
-        console.warn(`Label ${label.name} is missing column or title information`);
-        continue;
-      }
-
-      if (!values[info.column]) {
-        values[info.column] = info.value;
-      } else {
-        values[info.column] += `, ${info.value}`;
-      }
-    }
-
-    return values;
-  },
-  /**
-   * Assigns a person to the Monday.com task object based on their GitHub username/role
-   * @param {import('@octokit/webhooks-types').User} person
-   * @param {object} values - The current column values object to update
-   * @returns {object} - The updated column values object
-   */
-  assignPerson: (person, values) => {
-    if (!person?.login) {
-      console.warn("No person or login provided for assignment");
-      return;
-    }
-
-    const info = mondayPeople.get(person.login);
-
-    if (!info) {
-      console.warn(`Assignee ${person.login} not found in peopleMap`);
-      return;
-    }
-
-    if (!values[info.role]) {
-      values[info.role] = `${info.id}`;
-    } else {
-      values[info.role] += `, ${info.id}`;
-    }
-
-    return values;
-  },
+  removeLabel,
+  createLabelIfMissing,
+  callMonday,
+  getMondayID,
+  addSyncLine,
+  assignLabels,
+  assignPerson,
+  handleMilestone,
 };
--- .github/scripts/updateMondayDate.js
+++ .github/scripts/updateMondayDate.js
@@ -1,5 +1,5 @@
 // @ts-check
-const { callMonday } = require("./support/utils");
+const { callMonday, getMondayID, handleMilestone } = require("./support/utils");
 const { mondayBoard, mondayColumns } = require("./support/resources");
 
 // When a Milestone is added or updated:
@@ -11,63 +11,25 @@ module.exports = async ({ context }) => {
   const payload = /** @type {import('@octokit/webhooks-types').IssuesMilestonedEvent} */ (context.payload);
   const {
     body,
-    number: issueNumber,
+    number,
     milestone: {
       title: milestone
     }
   } = payload.issue;
 
   /**
-   * Returns the Monday.com task ID for the passed GitHub Issue ID.
-   * Matches based on the GitHub Issue ID column.
-   * @param {number} githubID 
-   * @returns {Promise<string>} 
+   * Update a column's value for a given Monday.com task.
+   * @param {string} ID - the ID of the Monday.com task
+   * @param {string} column - the ID of the column to update
+   * @param {string} value - the value to set for the column
    */
-  async function getMondayID(githubID) {
-    const query = `query {
-      items_page_by_column_values(
-        board_id: "${mondayBoard}",
-        columns: {
-          column_id: "${mondayColumns.issueNumber}",
-          column_values: ["${githubID}"]
-        },
-      ) {
-        items {
-          id
-        }
-      }
-    }`;
-
-    const response = await callMonday(MONDAY_KEY, query);
-
-    if (!response) {
-      throw new Error(`No response for Github Issue #${githubID}`);
-    }
-
-    const items = response["data"]["items_page_by_column_values"]["items"];
-    
-    if (!items?.length) {
-      throw new Error(`No items found for Github Issue #${githubID}`);
-    }
-
-    return items[0]["id"];
-  }
-
-  /**
-   * Update the Due Date column value for a Monday.com task.
-   * @param {string} ID 
-   * @param {string} date 
-   */
-  function updateDueDate(ID, date) {
-    const value = JSON.stringify({ "date": date });
-    const valueEscaped = JSON.stringify(value);
-
+  function updateColumnValue(ID, column, value) {
     const query = `mutation {
       change_column_value(
         board_id: "${mondayBoard}",
         item_id: "${ID}",
-        column_id: "${mondayColumns.date}",
-        value: ${valueEscaped}
+        column_id: "${column}",
+        value: ${value}
       ) {
         id
       }
@@ -76,25 +38,15 @@ module.exports = async ({ context }) => {
     callMonday(MONDAY_KEY, query);
   }
 
-  const dateRegex = /\d{4}-\d{2}-\d{2}/;
-  const dueDate = milestone.match(dateRegex);
-  const dateArgument = dueDate ? dueDate[0] : "";
-
   const mondayRegex = /(?<=\*\*monday\.com sync:\*\* #)(\d+)/;
   const mondayRegexMatch = body?.match(mondayRegex);
-  let foundMondayID = mondayRegexMatch && mondayRegexMatch[0] ? mondayRegexMatch[0] : "";
+  let mondayID = mondayRegexMatch && mondayRegexMatch[0] ? mondayRegexMatch[0] : "";
 
-  if (foundMondayID) {
-    updateDueDate(foundMondayID, dateArgument);
-  } else {
-    getMondayID(issueNumber).then(mondayID => {
-      updateDueDate(mondayID, dateArgument);
-    });
+  if (!mondayID) {
+    mondayID = await getMondayID(MONDAY_KEY, number);
   }
 
-  if (dateArgument) {
-    console.log(`Issue #${issueNumber}'s date updated to: ${dateArgument}`);
-  } else {
-    console.log(`Issue #${issueNumber}'s date has been cleared.`);
-  }
+  const { column, value } = handleMilestone(milestone);
+
+  updateColumnValue(mondayID, column, value);
 };
