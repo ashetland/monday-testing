3c59ac2355e6b10d85082bfc176f5615a61a8a7a
Isaac Braun <isaacbraun2000@gmail.com>
2025-07-28 18:57:17 -0700

    feat: handle unassigned action and update status when needed

--- .github/scripts/updateMondayAssignee.js
+++ .github/scripts/updateMondayAssignee.js
@@ -1,79 +1,84 @@
 // @ts-check
-const { callMonday, getMondayID } = require("./support/utils");
-const { mondayBoard, mondayPeople } = require("./support/resources");
+const { updateMultipleColumns, notReadyForDev, notInLifecycle } = require("./support/utils");
+const { mondayPeople, mondayLabels, resources } = require("./support/resources");
 
-// When a Milestone is added or updated:
-// 1. Find ID of task in Issue Body, if not found, find in Monday
-// 2. Update Due Date column value in Monday to Due Date of Milestone, or clear it if not found.
 /** @param {import('github-script').AsyncFunctionArguments} AsyncFunctionArguments */
 module.exports = async ({ context }) => {
   const { MONDAY_KEY } = process.env;
   const payload =
-    /** @type {import('@octokit/webhooks-types').IssuesAssignedEvent} */ (
+    /** @type {import('@octokit/webhooks-types').IssuesAssignedEvent | import('@octokit/webhooks-types').IssuesUnassignedEvent } */ (
       context.payload
     );
   const {
-    issue: { number, body, assignees: currentAssignees },
+    issue: { number, body, assignees: currentAssignees, labels },
     assignee: newAssignee,
+    action,
   } = payload;
 
-  console.log(`New Assignee: ${newAssignee}, ${newAssignee?.login}`);
-  if (!newAssignee) {
-    console.log(`No new assignee found for issue #${number}. Skipping update.`);
-    return;
-  }
-
-  const mondayID = await getMondayID(MONDAY_KEY, body, number);
-  if (!mondayID) {
-    console.log(`No Monday ID found for issue #${number}. Skipping update.`);
-    return;
-  }
-
-  const personInfo = mondayPeople.get(newAssignee.login);
-  if (!personInfo) {
-    console.log(
-      `No Monday person info found for assignee ${newAssignee.login}. Skipping update.`,
-    );
-    return;
-  }
-
-  let valueString = "";
-  currentAssignees.forEach((assignee) => {
-    if (assignee.login === newAssignee.login) {
+  /**
+    * @param {import('@octokit/webhooks-types').User} assignee
+    * @param {import('@octokit/webhooks-types').User[]} currentAssignees
+    * @param {object} values - Object to hold the values to be updated in Monday
+  */
+  function getAssigneeValue(assignee, currentAssignees, values) {
+    const personInfo = mondayPeople.get(assignee.login);
+    if (!personInfo) {
+      console.log(
+        `No Monday person info found for assignee ${assignee.login}. Skipping update.`,
+      );
       return;
     }
 
-    const info = mondayPeople.get(assignee.login);
-    if (info && info.role === personInfo.role) {
-      if (valueString) {
-        valueString += `, `;
+    currentAssignees.forEach((assignee) => {
+      if (assignee.login === assignee.login) {
+        return;
       }
 
-      valueString += `${info.id}`;
-    }
-  });
+      const info = mondayPeople.get(assignee.login);
+      if (info && info.role === personInfo.role) {
+        if (values[info.role]) {
+          values[info.role] += `, `;
+        }
 
-  if (valueString) {
-    valueString += `, ${personInfo.id}`;
-  } else {
-    valueString = `${personInfo.id}`;
+        values[info.role] += `${info.id}`;
+      }
+    });
+
+    if (values[personInfo.role]) {
+      values[personInfo.role] += `, ${personInfo.id}`;
+    } else {
+      values[personInfo.role] = `${personInfo.id}`;
+    }
+
+    return values;
   }
 
-  const query = `mutation {
-    change_simple_column_value (
-      board_id: ${mondayBoard},
-      item_id: ${mondayID},
-      column_id: "${personInfo.role}",
-      value: "${valueString}"
-    ) {
-      id
-    }
-  }`;
+  if (!newAssignee) {
+    throw new Error(`No new assignee found for issue #${number}.`);
+  }
 
-  const response = await callMonday(MONDAY_KEY, query);
-  if (!response || !response["data"] || !response["data"]["change_simple_column_value"]) {
-    throw new Error(
-      `Failed to update Monday board for issue #${number}. Response: ${JSON.stringify(response)}`,
-    );
+  let valueObject = {};
+  if (action === "unassigned" && notReadyForDev(labels)) {
+    const unassigned = mondayLabels.get(resources.labels.issueWorkflow.new);
+
+    if (unassigned) {
+      valueObject[unassigned.column] = unassigned.value;
+    }
+  } else {
+    if (notInLifecycle(labels)) {
+      const assigned = mondayLabels.get(resources.labels.issueWorkflow.assigned);
+
+      if (assigned) {
+        valueObject[assigned.column] = assigned.value;
+      }
+    }
+
+    valueObject = getAssigneeValue(newAssignee, currentAssignees, valueObject);
+  }
+
+  try {
+    await updateMultipleColumns(MONDAY_KEY, body, number, valueObject);
+  } catch (error) {
+    throw new Error(`Error updating assignee values in Monday.com: ${error}`);
   }
 };
