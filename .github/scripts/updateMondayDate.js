1af36c45f46ef0fbd015a7df2e2346b5f590d3bc
Isaac Braun <isaacbraun2000@gmail.com>
2025-07-28 18:56:23 -0700

    await updateMultipleColumns

--- .github/scripts/updateMondayDate.js
+++ .github/scripts/updateMondayDate.js
@@ -1,10 +1,6 @@
 // @ts-check
 const { handleMilestone, updateMultipleColumns } = require("./support/utils");
-const { mondayBoard } = require("./support/resources");
 
-// When a Milestone is added or updated:
-// 1. Find ID of task in Issue Body, if not found, find in Monday
-// 2. Update Due Date column value in Monday to Due Date of Milestone, or clear it if not found.
 /** @param {import('github-script').AsyncFunctionArguments} AsyncFunctionArguments */
 module.exports = async ({ context }) => {
   const { MONDAY_KEY } = process.env;
@@ -25,7 +21,7 @@ module.exports = async ({ context }) => {
   });
 
   try {
-    updateMultipleColumns(MONDAY_KEY, body, number, valuesObject);
+    await updateMultipleColumns(MONDAY_KEY, body, number, valuesObject);
   }
   catch (error) {
     throw new Error(`Error updating Milestone values in Monday.com: ${error}`);
