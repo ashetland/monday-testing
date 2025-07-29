9087b1079605ddf4761eac15d72e07ada75fceb6
Isaac Braun <isaacbraun2000@gmail.com>
2025-07-29 14:47:29 -0700

    update status based on assignee and labels

--- .github/scripts/createMondayTask.js
+++ .github/scripts/createMondayTask.js
@@ -1,21 +1,45 @@
 // @ts-check
-const { callMonday, addSyncLine, assignLabel, assignPerson, handleMilestone, formatValues } = require("./support/utils");
-const { mondayBoard, mondayColumns } = require("./support/resources");
+const {
+  callMonday,
+  addSyncLine,
+  assignLabel,
+  assignPerson,
+  handleMilestone,
+  formatValues,
+  notReadyForDev,
+  notInLifecycle,
+} = require("./support/utils");
+const {
+  mondayBoard,
+  mondayColumns,
+  mondayLabels,
+  resources,
+} = require("./support/resources");
 
 /** @param {import('github-script').AsyncFunctionArguments} AsyncFunctionArguments */
 module.exports = async ({ github, context }) => {
   const { MONDAY_KEY } = process.env;
   const payload =
-    /** @type {import('@octokit/webhooks-types').IssuesOpenedEvent} */ (
+    /** @type {import('@octokit/webhooks-types').IssuesOpenedEvent | import('@octokit/webhooks-types').IssuesLabeledEvent}*/ (
       context.payload
     );
-  const { title, body, number, labels, assignee, assignees, html_url, milestone } = payload.issue;
+  const {
+    title,
+    body,
+    number,
+    labels,
+    assignee,
+    assignees,
+    html_url,
+    milestone,
+  } = payload.issue;
 
   /**
    * Creates the GraphQL query to create a new item in Monday.com
    * @returns {string} - The GraphQL query string
    */
   function createTaskQuery() {
+    /** @type {Record<string, string | number | object>} */
     let values = {
       [mondayColumns.issueNumber]: `${number}`,
       [mondayColumns.link]: {
@@ -24,22 +48,38 @@ module.exports = async ({ github, context }) => {
       },
     };
 
-    if (assignees) {
-      assignees.forEach((person) => {
-        values = assignPerson(person, values);
-      });
-    }
-
     if (labels) {
       labels.forEach((label) => {
         values = assignLabel(label, values);
       });
     }
 
-    if (milestone) {
-      handleMilestone(milestone, assignee, labels).forEach(({ column, value }) => {
-        values[column] = value;
+    if (assignees) {
+      assignees.forEach((person) => {
+        values = assignPerson(person, values);
       });
+
+      if (notInLifecycle(labels)) {
+        const assigned = mondayLabels.get(
+          resources.labels.issueWorkflow.assigned,
+        );
+        if (assigned) {
+          values[assigned.column] = assigned.value;
+        }
+      }
+    } else if (notReadyForDev(labels)) {
+      const unassigned = mondayLabels.get(resources.labels.issueWorkflow.new);
+      if (unassigned) {
+        values[unassigned.column] = unassigned.value;
+      }
+    }
+
+    if (milestone) {
+      handleMilestone(milestone, assignee, labels).forEach(
+        ({ column, value }) => {
+          values[column] = value;
+        },
+      );
     }
 
     const query = `mutation { 
@@ -55,8 +95,38 @@ module.exports = async ({ github, context }) => {
     return query;
   }
 
+  // if (payload.action === "labeled") {
+  //   // Check if issue exists in Monday.com
+  //   const query = `query {
+  //     items_page_by_column_values(
+  //       board_id: "${mondayBoard}",
+  //       columns: {
+  //         column_id: "${mondayColumns.issueNumber}",
+  //         column_values: ["${number}"]
+  //       },
+  //     ) {
+  //       items {
+  //         id
+  //       }
+  //     }
+  //   }`;
+  //   const existingItemID = await callMonday(MONDAY_KEY, query);
+  //
+  //   if (
+  //     existingItemID &&
+  //     existingItemID["data"] &&
+  //     existingItemID["data"]["items_page_by_column_values"]["items"].length > 0
+  //   ) {
+  //     // Item already exists, no need to create a new one
+  //     return;
+  //   }
+  // } else {
   const response = await callMonday(MONDAY_KEY, createTaskQuery());
-  if (!response || !response["data"] || !response["data"]["create_item"]["id"]) {
+  if (
+    !response ||
+    !response["data"] ||
+    !response["data"]["create_item"]["id"]
+  ) {
     throw new Error(`Missing or bad response for Github Issue #${number}`);
   }
 
