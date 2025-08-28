bf09b16678b119b6867a1a3733d8b947721e017f
Isaac Braun <isaacbraun2000@gmail.com>
2025-08-27 17:09:23 -0700

    when labeled, check if Monday task exists

--- .github/scripts/mondayCreateTask.js
+++ .github/scripts/mondayCreateTask.js
@@ -3,11 +3,19 @@ const Monday = require("./support/monday.js");
 
 /** @param {import('github-script').AsyncFunctionArguments} AsyncFunctionArguments */
 module.exports = async ({ github, context }) => {
-  const { issue } =
+  const { issue, action } =
     /** @type {import('@octokit/webhooks-types').IssuesOpenedEvent | import('@octokit/webhooks-types').IssuesLabeledEvent}*/ (
       context.payload
     );
   const monday = Monday(issue);
+
+  if (action === "labeled") {
+    const id = await monday.getId();
+    if (id) {
+      console.log(`Issue already exists in Monday.com. ID: ${id}.`);
+      process.exit(0);
+    }
+  }
 
   const id = await monday.createTask();
 
