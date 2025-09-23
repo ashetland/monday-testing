c79d0e50b7e49d40859cde1dcfdeb4e158a2e283
Isaac Braun <isaacbraun2000@gmail.com>
2025-09-23 14:11:38 -0700

    add @ and clear assignees

--- .github/scripts/notifyWhenInstalled.js
+++ .github/scripts/notifyWhenInstalled.js
@@ -13,15 +13,25 @@ module.exports = async ({ github, context }) => {
 
   const { ISSUE_VERIFIERS } = process.env;
 
-  await removeLabel({ github, context, label: issueWorkflow.inDevelopment });
-
-  const verifiers = ISSUE_VERIFIERS?.split(",").map((v) => v.trim());
-
-  await github.rest.issues.createComment({
+  const issueProps = {
     owner,
     repo,
     issue_number: number,
-    body: `Installed for verification: ${verifiers}`,
+  };
+
+  await removeLabel({ github, context, label: issueWorkflow.inDevelopment });
+
+  // Add a "@" character to notify the user
+  const verifiers = ISSUE_VERIFIERS?.split(",").map((v) => " @" + v.trim());
+
+  await github.rest.issues.update({
+    ...issueProps,
+    assignees: [],
+  });
+
+  await github.rest.issues.createComment({
+    ...issueProps,
+    body: `Installed for verification:${verifiers}`,
   });
 };
 
