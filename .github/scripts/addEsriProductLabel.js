06dbfb91a9a4e691bf59aa3163462e5def4e7356
Isaac Braun <isaacbraun2000@gmail.com>
2025-11-14 17:30:10 -0800

    test new product regex

--- .github/scripts/addEsriProductLabel.js
+++ .github/scripts/addEsriProductLabel.js
@@ -2,44 +2,36 @@
 const { createLabelIfMissing } = require("./support/utils");
 
 /** @param {import('github-script').AsyncFunctionArguments} AsyncFunctionArguments */
-module.exports = async ({ github, context }) => {
+module.exports = async ({ github, context, core }) => {
   const { repo, owner } = context.repo;
+  const logParams = { title: "Add Esri Product Label" };
 
-  const payload = /** @type {import('@octokit/webhooks-types').IssuesEvent} */ (context.payload);
+  const payload = /** @type {import('@octokit/webhooks-types').IssuesEvent} */ (
+    context.payload
+  );
   const {
-    action,
     issue: { body, number: issue_number },
   } = payload;
 
   if (!body) {
-    console.log("could not determine the issue body");
+    core.notice("Could not determine the issue body", logParams);
     return;
   }
 
-  const productRegex = new RegExp(
-    action === "edited"
-      ? // the way GitHub parses the issue body into plaintext
-        // requires this exact format for edits
-        "(?<=### Esri team\n\n).+"
-      : // otherwise it depends on the submitter's OS
-        "(?<=### Esri team[\r\n|\r|\n]{2}).+$",
-    "m",
-  );
-
+  const productRegex = /### Esri team[\r|\n]+(.+)$/m;
   const productRegexMatch = body.match(productRegex);
-  console.log(`Esri team regex match: ${productRegexMatch}`, JSON.stringify(body));
+  const product = (productRegexMatch?.[0] || "").trim();
 
   // If issue includes "Esri team" line then create label, otherwise log message.
-  if (productRegexMatch) {
-    const product = (productRegexMatch && productRegexMatch[0] ? productRegexMatch[0] : "").trim();
-    const color = "006B75";
-
+  if (product === "") {
+    core.notice(`No Esri team listed on issue #${issue_number}`, logParams);
+  } else {
     if (product !== "N/A") {
       await createLabelIfMissing({
         github,
         context,
         label: product,
-        color: color,
+        color: "006B75",
         description: `Issues logged by ${product} team members.`,
       });
 
@@ -54,17 +46,14 @@ module.exports = async ({ github, context }) => {
         owner,
         repo,
         workflow_id: "issue-monday-sync.yml",
-        ref: "main",
+        ref: "dev",
         inputs: {
           issue_number: issue_number.toString(),
           event_type: "SyncActionChanges",
           label_name: product,
-          label_color: color,
-          label_action: "added"
+          label_action: "added",
         },
       });
     }
-  } else {
-    console.log(`No Esri team listed on issue #${issue_number}`);
   }
 };
